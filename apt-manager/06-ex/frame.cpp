4672f1868353b25e18efb0f0bb322a4641a6000e
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 05:40:05 -0300

    :add: Adiciona o arquivo apt-manager/06-ex/frame.cpp

---
+++ apt-manager/06-ex/frame.cpp
@@ -0,0 +1,65 @@
+#include <wx/wx.h>
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+
+private:
+    void OnQuit (wxCommandEvent&);
+    void OnAbout (wxCommandEvent&);
+};
+
+enum
+{
+    ID_Hello = wxID_HIGHEST + 1
+};
+
+MyFrame::MyFrame()
+    : wxFrame (nullptr, wxID_ANY,
+               "Exemplo wxWidgets - Arquivos separados",
+               wxDefaultPosition, wxSize (600, 400))
+{
+    // --- Menu ---
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append (ID_Hello, "&Hello...\tCtrl-H", "Diga olá");
+    menuFile->AppendSeparator();
+    menuFile->Append (wxID_EXIT);
+
+    wxMenu* menuHelp = new wxMenu;
+    menuHelp->Append (wxID_ABOUT);
+
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append (menuFile, "&File");
+    menuBar->Append (menuHelp, "&Help");
+    SetMenuBar (menuBar);
+
+    // --- Status bar ---
+    CreateStatusBar();
+    SetStatusText ("Bem-vindo ao wxWidgets!");
+
+    // --- Eventos ---
+    Bind (wxEVT_MENU, &MyFrame::OnQuit,  this, wxID_EXIT);
+    Bind (wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind (wxEVT_MENU, [this] (wxCommandEvent&)
+    {
+        wxLogMessage ("Olá de wxWidgets!");
+    }, ID_Hello);
+}
+
+void MyFrame::OnQuit (wxCommandEvent&)
+{
+    Close (true);
+}
+
+void MyFrame::OnAbout (wxCommandEvent&)
+{
+    wxMessageBox ("Exemplo básico com wxWidgets\nArquivos: app.cpp e frame.cpp",
+                  "Sobre", wxOK | wxICON_INFORMATION, this);
+}
+
+// Factory function visível para app.cpp
+wxFrame* CreateMainFrame()
+{
+    return new MyFrame();
+}

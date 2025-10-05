58950629645916d9f8a3aae9acd531161d53ea4a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 05:40:04 -0300

    :add: Adiciona o arquivo apt-manager/05-ex/app.cpp

---
+++ apt-manager/05-ex/app.cpp
@@ -0,0 +1,22 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame() : wxFrame (nullptr, wxID_ANY, "Olá, wxWidgets!") {}
+};
+
+wxIMPLEMENT_APP (MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame();
+    frame->Show (true);
+    return true;
+}

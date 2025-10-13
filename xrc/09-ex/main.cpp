a195b0886294cd6bd573348ba3438033c88f0485
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-13 14:07:36 -0300

    :style: Atualiza o estilo do arquivo xrc/09-ex/main.cpp

--- xrc/09-ex/main.cpp
+++ xrc/09-ex/main.cpp
@@ -1,7 +1,10 @@
-#include <wx/button.h>
-#include <wx/listctrl.h>
-#include <wx/textctrl.h>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
 #include <wx/wx.h>
+#endif
+
+#include <wx/wizard.h>
+#include <wx/xrc/xh_all.h> // garante handlers, inclusive wizard
 #include <wx/xrc/xmlres.h>
 
 class MyApp : public wxApp {
@@ -14,13 +17,38 @@ public:
       return false;
     }
 
-    wxFrame *frame = wxXmlResource::Get()->LoadFrame(nullptr, "ID_WXFRAME");
-    if (!frame) {
-      wxLogError("Não foi possível instanciar o frame ID_WXFRAME do XRC.");
+    // 1) Cria a instância vazia do wizard
+    wxWizard *wiz = new wxWizard(); // construtor vazio
+
+    // 2) Popula a instância a partir do XRC (class deve ser "wxWizard")
+    if (!wxXmlResource::Get()->LoadObject(wiz, nullptr, "ID_WXWIZARD",
+                                          "wxWizard")) {
+      wxLogError(
+          "Não foi possível instanciar 'ID_WXWIZARD' como wxWizard (XRC).");
+      wiz->Destroy();
       return false;
     }
 
-    frame->Show(true);
+    // 3) Tentar descobrir uma primeira página automaticamente
+    wxWizardPage *firstPage = nullptr;
+    for (wxWindowList::compatibility_iterator node =
+             wiz->GetChildren().GetFirst();
+         node; node = node->GetNext()) {
+      if (auto *page = wxDynamicCast(node->GetData(), wxWizardPage)) {
+        firstPage = page;
+        break;
+      }
+    }
+
+    // 4) Executa o wizard do jeito certo, se achou página inicial
+    if (firstPage) {
+      // RunWizard exibe modal até concluir/cancelar
+      wiz->RunWizard(firstPage);
+      wiz->Destroy();
+    } else {
+      // fallback: mostra não-modal (útil durante montagem do XRC)
+      wiz->Show(true);
+    }
     return true;
   }
 };

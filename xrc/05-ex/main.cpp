5ba1672053ed7c7d6c771531e56c6a69099879f2
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-13 11:36:27 -0300

    :add: Adiciona o arquivo xrc/05-ex/main.cpp

---
+++ xrc/05-ex/main.cpp
@@ -0,0 +1,101 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include <wx/button.h>
+#include <wx/listctrl.h>
+#include <wx/textctrl.h>
+#include <wx/xrc/xh_all.h>  // handlers XRC
+#include <wx/xrc/xmlres.h>
+
+class MyApp : public wxApp
+{
+public:
+    bool OnInit() override
+    {
+        // 1) registra todos os handlers de XRC (botões, sizers, listctrl, etc.)
+        wxXmlResource::Get()->InitAllHandlers();
+        // redundante mas útil em alguns builds: força o link dos handlers
+
+        // 2) carrega o arquivo .xrc
+        if (!wxXmlResource::Get()->Load("ui.xrc"))
+        {
+            wxMessageBox("Falha ao carregar ui.xrc (confira o caminho e o "
+                         "working directory).",
+                         "Erro",
+                         wxOK | wxICON_ERROR);
+            return false;
+        }
+
+        // 3) cria o frame definido no XRC (name="ID_WXFRAME")
+        wxFrame* frame = wxXmlResource::Get()->LoadFrame(nullptr, "ID_WXFRAME");
+        if (!frame)
+        {
+            wxMessageBox(
+                "Falha ao instanciar ID_WXFRAME do XRC (handlers faltando?).",
+                "Erro",
+                wxOK | wxICON_ERROR);
+            return false;
+        }
+
+        // wxButton* btnRefresh;         // Botão "Atualizar Lista"
+        // wxButton* btnUpdate;          // Botão "APT Update"
+        // wxButton* btnUpgrade;         // Botão "APT Upgrade"
+        // wxButton* btnListUpgradable;  // Botão "Pacotes Atualizáveis"
+
+        auto* btnRefresh = XRCCTRL(*frame, "BTN_REFRESH", wxButton);
+        if (btnRefresh)
+        {
+            btnRefresh->SetLabel("Atualizar Lista");
+        }
+
+        auto* btnUpdate = XRCCTRL(*frame, "BTN_UPDATE", wxButton);
+        if (btnUpdate)
+        {
+            btnUpdate->SetLabel("APT Update");
+        }
+
+        auto* btnUpgrade = XRCCTRL(*frame, "BTN_UPGRADE", wxButton);
+        if (btnUpgrade)
+        {
+            btnUpgrade->SetLabel("APT Upgrade");
+        }
+
+        auto* btnListUpgradable =
+            XRCCTRL(*frame, "BTN_LIST_UPGRADABLE", wxButton);
+        if (btnListUpgradable)
+        {
+            btnListUpgradable->SetLabel(
+                // garante UTF-8
+                wxString::FromUTF8(u8"Pacotes Atualizáveis"));
+        }
+
+        // -------------------------------------------------------------------
+
+        auto* staticText1 = XRCCTRL(*frame, "ID_STATIC_TEXT_1", wxStaticText);
+        if (staticText1)
+        {
+            staticText1->SetLabel(
+                // garante UTF-8
+                wxString::FromUTF8(
+                    u8"Pacotes Atualizáveis k k k k k k k k k "));
+        }
+
+        auto* staticText2 = XRCCTRL(*frame, "ID_STATIC_TEXT_2", wxStaticText);
+        if (staticText2)
+        {
+            staticText2->SetLabel(
+                // garante UTF-8
+                wxString::FromUTF8(
+                    u8"Pacotes Atualizáveis t t t t t t t t t "));
+        }
+
+        frame->Layout();  // recalcula sizers após mudar labels
+        frame->Show(true);
+
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);

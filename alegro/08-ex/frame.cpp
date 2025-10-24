b8e357e1487425edb6956f8b66876c6b875346a7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-24 00:44:32 -0300

    :style: Atualiza o estilo do arquivo alegro/08-ex/frame.cpp

--- alegro/08-ex/frame.cpp
+++ alegro/08-ex/frame.cpp
@@ -2,30 +2,30 @@
 #include "sdl_panel.h"
 #include <wx/sizer.h>
 
-namespace {
+namespace
+{
 constexpr int  kW = 800;
 constexpr int  kH = 500;
 constexpr auto kTitle = "wxWidgets + SDL2 dentro de wxPanel";
 }
 
 MyFrame::MyFrame()
-: wxFrame(nullptr, wxID_ANY, kTitle, wxDefaultPosition, wxSize{kW, kH})
+    : wxFrame (nullptr, wxID_ANY, kTitle, wxDefaultPosition, wxSize{kW, kH})
 {
     CreateStatusBar();
-    SetStatusText("Pronto para renderizar com SDL dentro do wxPanel.");
+    SetStatusText ("Pronto para renderizar com SDL dentro do wxPanel.");
 
-    auto* root  = new wxPanel(this);
-    auto* sizer = new wxBoxSizer(wxVERTICAL);
+    auto* root  = new wxPanel (this);
+    auto* sizer = new wxBoxSizer (wxVERTICAL);
 
-    auto* sdlPanel = new SDLPanel(root);
-    sizer->Add(sdlPanel, 1, wxEXPAND | wxALL, 8);
+    auto* sdlPanel = new SDLPanel (root);
+    sizer->Add (sdlPanel, 1, wxEXPAND | wxALL, 8);
 
-    root->SetSizer(sizer);
+    root->SetSizer (sizer);
 
-    auto* frameSizer = new wxBoxSizer(wxVERTICAL);
-    frameSizer->Add(root, 1, wxEXPAND);
-    SetSizer(frameSizer);
+    auto* frameSizer = new wxBoxSizer (wxVERTICAL);
+    frameSizer->Add (root, 1, wxEXPAND);
+    SetSizer (frameSizer);
 
     Centre();
 }
-

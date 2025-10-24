3dd9c9443dbf7a88ed2a169fd43ac8dd6f2b033e
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-24 02:38:30 -0300

    :add: Adiciona o arquivo alegro/14-ex/core/BackendFactory.hpp

---
+++ alegro/14-ex/core/BackendFactory.hpp
@@ -0,0 +1,15 @@
+// core/BackendFactory.hpp
+#pragma once
+#include <memory>
+#include "IRenderBackend.hpp"
+
+namespace core {
+
+enum class RenderBackendType {
+    Allegro
+};
+
+std::unique_ptr<IRenderBackend> makeBackend(RenderBackendType type);
+
+} // namespace core
+

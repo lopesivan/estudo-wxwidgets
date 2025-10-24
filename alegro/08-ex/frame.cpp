80309bfe934fdef27e546e228a7127d5d01de4aa
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 23:35:38 -0300

    :add: Adiciona o arquivo alegro/08-ex/frame.cpp

---
+++ alegro/08-ex/frame.cpp
@@ -0,0 +1,30 @@
+#include "frame.h"
+#include "sdl_panel.h"
+#include <wx/sizer.h>
+
+namespace {
+    constexpr int  kW = 800;
+    constexpr int  kH = 500;
+    constexpr auto kTitle = "wxWidgets + SDL2 dentro de wxPanel";
+}
+
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, kTitle, wxDefaultPosition, wxSize{kW, kH})
+{
+    CreateStatusBar();
+    SetStatusText("Pronto para renderizar com SDL dentro do wxPanel.");
+
+    auto* root = new wxPanel(this);
+    auto* sizer = new wxBoxSizer(wxVERTICAL);
+
+    // Painel onde o SDL vai desenhar
+    auto* sdlPanel = new SDLPanel(root); // SDL inicializa quando o painel estiver pronto
+    sizer->Add(sdlPanel, 1, wxEXPAND | wxALL, 8);
+
+    root->SetSizerAndFit(sizer);
+
+    auto* frameSizer = new wxBoxSizer(wxVERTICAL);
+    frameSizer->Add(root, 1, wxEXPAND);
+    SetSizerAndFit(frameSizer);
+}
+

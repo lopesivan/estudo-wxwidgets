61f8811156ccd9d2dec6cdbf01f3222d69a6f6d2
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 21:11:20 -0300

    :add: Adiciona o arquivo alegro/02-ex/app.cpp

---
+++ alegro/02-ex/app.cpp
@@ -0,0 +1,13 @@
+#include "app.h"
+#include "frame.h"
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    wxInitAllImageHandlers();
+
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+

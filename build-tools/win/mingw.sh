59fc0a04b5ce3dbc98fcf6c3277c19f9992691ae
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-01 16:07:38 -0300

    :add: Adiciona o arquivo build-tools/win/mingw.sh

---
+++ build-tools/win/mingw.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# if exist path `build' then remove.
+_d=build-mingw64
+test -d $_d && rm -rf $_d
+mkdir $_d
+cd $_d || exit
+
+MINGW64="/c/mingw64"
+
+# Adiciona o binário do MinGW ao PATH
+export PATH="$MINGW64/bin:$PATH"
+
+echo ""
+echo "✅ Ambiente MinGW-w64 ativado!"
+echo "👉 GCC em: $MINGW64/bin"
+echo "🛠 Versão:"
+gcc --version
+echo ""
+
+cmake -S . \
+    -G "MinGW Makefiles" \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D wxBUILD_SHARED=ON \
+    -D wxBUILD_USE_STATIC_RUNTIME=OFF \
+    -D wxBUILD_SAMPLES=OFF \
+    -D wxBUILD_TESTS=OFF \
+    -D wxUSE_WEBVIEW=OFF \
+    -D wxUSE_WEBVIEW_EDGE=OFF \
+    -D CMAKE_MAKE_PROGRAM="C:/mingw64/bin/mingw32-make.exe" \
+    -D CMAKE_C_COMPILER="C:/mingw64/bin/gcc.exe" \
+    -D CMAKE_CXX_COMPILER="C:/mingw64/bin/g++.exe" \
+    -D CMAKE_INSTALL_PREFIX="C:/Users/ivan/source/repos/SDKs/MinGW64/wx-3.3.1-gcc-x64" ..
+
+/c/mingw64/bin/mingw32-make.exe
+
+exit 0

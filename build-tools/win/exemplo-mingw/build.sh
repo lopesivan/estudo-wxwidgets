0a2d8fab0c914ad3bf9bc75f000dbff3518e118f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-01 16:58:31 -0300

    :style: Atualiza o estilo do arquivo build-tools/win/exemplo-mingw/build.sh

--- build-tools/win/exemplo-mingw/build.sh
+++ build-tools/win/exemplo-mingw/build.sh
@@ -3,6 +3,7 @@
 # if exist path `build' then remove.
 _d=build-mingw64
 test -d $_d && rm -rf $_d
+mkdir $_d
 
 MINGW64="/c/mingw64"
 
@@ -16,7 +17,7 @@ echo "🛠 Versão:"
 gcc --version
 echo ""
 
-cmake -S . -B build-mingw -G "MinGW Makefiles" \
+cmake -S . -B $_d -G "MinGW Makefiles" \
     -D CMAKE_BUILD_TYPE=Release \
     -D wxUSE_WEBVIEW=OFF \
     -D wxUSE_WEBVIEW_EDGE=OFF \
@@ -25,6 +26,5 @@ cmake -S . -B build-mingw -G "MinGW Makefiles" \
     -D CMAKE_CXX_COMPILER="C:/mingw64/bin/g++.exe" \
     -D wxWidgets_DIR="C:/Users/ivan/source/repos/SDKs/MinGW64/wx-3.3.1-gcc-x64/lib/cmake/wxWidgets"
 
-/c/mingw64/bin/mingw32-make.exe -C build-mingw
+/c/mingw64/bin/mingw32-make.exe -C $_d
 
-exit 0

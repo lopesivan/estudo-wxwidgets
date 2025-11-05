f420daf43d2ee60dd32011021f81e7c65164aebc
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-05 03:38:14 -0300

    :add: Adiciona o arquivo console/length.cpp

---
+++ console/length.cpp
@@ -0,0 +1,8 @@
+#include <wx/string.h>
+#include <wx/crt.h>
+
+int main(int argc, char** argv)
+{
+    wxString str = wxT("The history of my life");
+    wxPrintf(wxT("The string has %d characters\n"), str.Len());
+}

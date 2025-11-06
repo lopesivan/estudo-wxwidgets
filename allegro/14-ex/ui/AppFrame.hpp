5a376c65dfb34bd780cad9d74e409756a43f9eda
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-06 03:46:40 -0300

    :add: Adiciona o arquivo allegro/14-ex/ui/AppFrame.hpp

---
+++ allegro/14-ex/ui/AppFrame.hpp
@@ -0,0 +1,34 @@
+// ui/AppFrame.hpp
+#pragma once
+#include <wx/wx.h>
+
+// #include <memory>
+
+#include "core/BackendFactory.hpp"
+#include "ui/RenderView.hpp"
+
+namespace ui
+{
+
+class AppFrame : public wxFrame
+{
+public:
+    AppFrame()
+        : wxFrame(
+              nullptr,
+              wxID_ANY,
+              "Bridge: wx (Abstraction) + Allegro (Implementor)",
+              wxDefaultPosition,
+              wxSize(1280, 720))
+    {
+        auto backend =
+            core::makeBackend(core::RenderBackendType::Allegro);
+        auto* view = new ui::RenderView(this, std::move(backend));
+        auto* sizer = new wxBoxSizer(wxVERTICAL);
+        sizer->Add(view, 1, wxEXPAND | wxALL, 8);
+        SetSizer(sizer);
+        Centre();
+    }
+};
+
+}  // namespace ui

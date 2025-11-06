f6211064c73f80f3cebab99d54f6900837dd22b3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-06 03:46:46 -0300

    :add: Adiciona o arquivo allegro/17-ex/ui/App.cpp

---
+++ allegro/17-ex/ui/App.cpp
@@ -0,0 +1,17 @@
+// ui/App.cpp
+#include <wx/wx.h>
+
+#include "ui/AppFrame.hpp"
+
+class MyApp : public wxApp
+{
+  public:
+    bool OnInit() override
+    {
+        auto* f = new ui::AppFrame();
+        f->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);

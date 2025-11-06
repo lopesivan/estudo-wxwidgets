69e4276179ddb7c5d70bba51a41dc5993acb4d21
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-06 01:40:40 -0300

    :add: Adiciona o arquivo advanced/wxBaiduNetdiskGui/wxBaiduNetDiskApp.h

---
+++ advanced/wxBaiduNetdiskGui/wxBaiduNetDiskApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      wxBaiduNetDiskApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2024-10-27
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef WXBAIDUNETDISKAPP_H
+#define WXBAIDUNETDISKAPP_H
+
+#include <wx/app.h>
+
+class wxBaiduNetDiskApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // WXBAIDUNETDISKAPP_H

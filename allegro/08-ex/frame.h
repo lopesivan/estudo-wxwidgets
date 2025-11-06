e6a079c085b3547e8dfec2b62851faf481dafed4
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-06 03:46:35 -0300

    :add: Adiciona o arquivo allegro/08-ex/frame.h

---
+++ allegro/08-ex/frame.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <wx/wx.h>
+
+class SDLPanel;
+
+class MyFrame final : public wxFrame {
+public:
+    MyFrame();
+
+    MyFrame (const MyFrame&)            = delete;
+    MyFrame& operator= (const MyFrame&) = delete;
+    MyFrame (MyFrame&&)                 = delete;
+    MyFrame& operator= (MyFrame&&)      = delete;
+};
+

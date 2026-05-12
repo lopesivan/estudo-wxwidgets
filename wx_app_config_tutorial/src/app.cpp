f4449b289781e1a9f51bedbf9cf716779da4893d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-05-12 11:26:35 -0300

    :style: Atualiza o estilo do arquivo wx_app_config_tutorial/src/app.cpp

--- wx_app_config_tutorial/src/app.cpp
+++ wx_app_config_tutorial/src/app.cpp
@@ -9,10 +9,13 @@ bool App::OnInit()
 {
     SetAppName("SettingsDemo");
 
-    auto *config = new wxFileConfig(
-        GetAppName(),
-        wxEmptyString, wxEmptyString, wxEmptyString,
-        wxCONFIG_USE_LOCAL_FILE | wxCONFIG_USE_SUBDIR | wxCONFIG_USE_XDG);
+    auto* config = new wxFileConfig(GetAppName(),
+                                    wxEmptyString,
+                                    wxEmptyString,
+                                    wxEmptyString,
+                                    wxCONFIG_USE_LOCAL_FILE |
+                                        wxCONFIG_USE_SUBDIR |
+                                        wxCONFIG_USE_XDG);
 
     wxConfigBase::Set(config);
 
@@ -21,19 +24,21 @@ bool App::OnInit()
     bool firstRun = !config->Read("/App/Version", &savedVersion);
     config->Write("/App/Version", wxString("1.0.0"));
 
-    auto *frame = new MainFrame();
+    auto* frame = new MainFrame();
     frame->Show();
 
-    if (firstRun)
+    if(firstRun)
     {
-        wxString configPath = config->GetLocalFileName(GetAppName(),
-            wxCONFIG_USE_SUBDIR | wxCONFIG_USE_XDG);
+        wxString configPath = config->GetLocalFileName(
+            GetAppName(), wxCONFIG_USE_SUBDIR | wxCONFIG_USE_XDG);
 
         wxMessageBox("Welcome to Settings Demo!\n\n"
                      "Open a text file to get started.\n"
                      "Your settings are stored in:\n" +
                          configPath,
-                     "Welcome", wxOK | wxICON_INFORMATION, frame);
+                     "Welcome",
+                     wxOK | wxICON_INFORMATION,
+                     frame);
     }
 
     return true;
